5f1bc1097fc648ae1f34764bd53aaaaad0d6033b
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 16:43:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+if katz_deli.length == 0 
+puts "The line is currently empty."
+elsif katz_deli.length > 0
+  katz_deli.push("#{katz_deli.length}. #{katz_deli}.")
+  puts "The line is currently: #{katz_deli.join}."
+end
+end
