c656da6d1dca15f0aecc8675b06d82465ef23fcb
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 21:47:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ end
 
 def now_serving(array)
     if array.length >=1
-     array.unshift(array)
+     array.shift(array)
      puts "Currently serving #{array}."
     else
      puts "There is nobody waiting to be served!"
