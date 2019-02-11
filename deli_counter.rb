eb6f9ea7e3a1785eb691409621503398456671ff
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 20:18:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katz_deli)
 if katz_deli.length == 0 
 puts "The line is currently empty."
 elsif katz_deli.length > 0
-  katz_deli.push do |people, index| 
-   "The line is currently: #{index+1}. #{people}."
+  katz_deli.push do |names
+   "The line is currently: #{names+1}. #{names}."
 end 
 end
 end
