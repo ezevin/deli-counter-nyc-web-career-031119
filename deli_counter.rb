00a0ebbb1607ad4c9ad5ab331463b978a8e43aaf
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 21:21:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,20 @@ other_deli = []
 end
 
 
+def line(deli_counter)
+  if deli_counter.length == 0 
+  puts "The line is currently empty."
+  elsif deli_counter.length >=1
+    new_count = []
+    counter = 1
+    deli_counter.each do |name| 
+    other_deli.push("#{counter}. #{name}")
+    counter+=1
+  end 
+    puts "The line is currently: #{new_count.join("")}"
+  end
+end
+
 def line(array) 
   if array.length >= 1
     nuarray = []
