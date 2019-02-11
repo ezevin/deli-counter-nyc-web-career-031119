47a95bdc35aef7e0bc178a608287547fa13eaf88
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 21:18:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,11 +20,3 @@ def line(deli_counter)
   end
 end
 
-if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
