169bc6aeb55b4454841f5d354c26cdecb9133e06
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 21:37:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,5 +27,5 @@ def take_a_number(array, name)
     array.push(name)
     counter += 1 
   end 
-  puts "Welcome, #{name}. You are number #{counter} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
