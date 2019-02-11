8a7abc7708064301bd2a474e93b2e780fc710e6d
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 21:01:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,8 +9,9 @@ end
 def line(deli_counter)
   if deli_counter.length == 0 
   puts "The line is currently empty."
-  elsif deli_counter.length > 0
+  while deli_counter.length > 0
     i = 0
+    line = deli_counter.length
     deli_counter.each_with_index.map do |people, index| 
     line = "#{index+1}. #{people}."
       line.map do |line|
